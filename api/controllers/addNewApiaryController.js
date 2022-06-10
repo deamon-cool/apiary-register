e7c9dc31329a05c835fd2ea1fbaf8f2faa82d48a
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-10 19:55:05 +0200

    created add new apiary controller

---
+++ api/controllers/addNewApiaryController.js
@@ -0,0 +1,16 @@
+const Apiary = require('../database/Apiary/Apiary');
+
+let apiaries = [];
+
+module.exports = (req, res) => {
+  try {
+    const { name, date, apiaryNumber } = req.body;
+
+    apiaries.push(new Apiary(name, date, apiaryNumber));
+
+    res.status.status(200).send({ info: 'Dodano pasiekę.' });
+  } catch (err) {
+    console.log(Date() + ' an error has occured \n' + err);
+    return res.status(500).send({ error: 'Błąd api. Przepraszamy.' });
+  }
+}
