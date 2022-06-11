05c9660f3eefaceb7e8d0dc0155ab63b64c53624
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 18:14:47 +0200

    changed directory

--- view/src/index.js
+++ view/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './component/App/App';
+import App from './component/stateful/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
