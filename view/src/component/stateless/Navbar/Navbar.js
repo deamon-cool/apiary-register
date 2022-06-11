5ff363c554a66d0cf38e88d81be35089c9d987e6
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 18:53:04 +0200

    imported image to Navbar, added tags and added click handler to image

--- view/src/component/stateless/Navbar/Navbar.js
+++ view/src/component/stateless/Navbar/Navbar.js
@@ -1,9 +1,17 @@
 import classes from './Navbar.module.css';
 
+import apiaryImage from '../../../assests/images/apiary.png';
+
 export default function Navbar(props) {
+  const imageClickHanlder = () => {
+    props.onImageClick();
+  }
+
   return (
     <nav className={classes.Navbar}>
+      <img src={apiaryImage} alt='Pasieka' onClick={imageClickHanlder} />
       <h1>Rejestr pasiek</h1>
+      <div></div>
     </nav>
   );
 }
