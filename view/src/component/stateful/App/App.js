9b48213e13b410cbd7066951834f57b670d337be
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 19:39:42 +0200

    imported container to app

--- view/src/component/stateful/App/App.js
+++ view/src/component/stateful/App/App.js
@@ -1,11 +1,15 @@
 import classes from './App.module.css';
 
 import Navbar from '../../stateless/Navbar/Navbar';
+import Container from '../../stateless/Container/Container';
 
 function App() {
   return (
     <div className={classes.App}>
       <Navbar />
+      <Container>
+        
+      </Container>
     </div>
   );
 }
