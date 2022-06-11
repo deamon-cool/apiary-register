a5c8716933f6b7d62bdbc181334384b7ea9b7d04
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 20:41:21 +0200

    created new component for routes

---
+++ view/src/component/stateless/RoutesContainer/RoutesContainer.js
@@ -0,0 +1,14 @@
+import classes from './RoutesContainer.module.css';
+
+import ImageButton from '../ImageButton/ImageButton';
+import newApiaryImage from '../../../../assests/images/new-apiary.png';
+import apiaryListImage from '../../../../assests/images/apiary-list.png';
+
+export default function RoutesContainer(props) {
+  return (
+    <main className={classes.RoutesContainer}>
+      <ImageButton title='Nowa pasieka' imageSrc={newApiaryImage} customStyle={{ margin: '10px' }} />
+      <ImageButton title='Lista pasiek' imageSrc={apiaryListImage} customStyle={{ margin: '10px' }} />
+    </main>
+  );
+}
