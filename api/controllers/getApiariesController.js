e85691de6068de734ddf151a451b47f8aff5ef0e
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 10:07:23 +0200

    implemented getting data from database in get apiary controller

--- api/controllers/getApiariesController.js
+++ api/controllers/getApiariesController.js
@@ -1,7 +1,9 @@
-const apiaries = require('../controllers/addNewApiaryController').apiaries;
+const Apiary = require('../database/Apiary/Apiary');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   try {
+    const apiaries = await Apiary.find();
+
     res.status(200).send({ apiaries: apiaries });
   } catch (err) {
     console.log(Date() + ' an error has occured \n' + err);
