c894b0d046d1381848c40ce5a67181f0254fc758
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 20:02:37 +0200

    added properties to components

--- view/src/component/stateful/App/App.js
+++ view/src/component/stateful/App/App.js
@@ -3,14 +3,16 @@ import classes from './App.module.css';
 import Navbar from '../../stateless/Navbar/Navbar';
 import Container from '../../stateless/Container/Container';
 import ImageButton from '../../stateless/ImageButton/ImageButton';
+import newApiaryImage from '../../../assests/images/new-apiary.png';
+import apiaryListImage from '../../../assests/images/apiary-list.png';
 
 function App() {
   return (
     <div className={classes.App}>
       <Navbar />
       <Container>
-        <ImageButton />
-        <ImageButton />
+        <ImageButton title='Nowa pasieka' imageSrc={newApiaryImage}/>
+        <ImageButton title='Lista pasiek' imageSrc={apiaryListImage}/>
       </Container>
     </div>
   );
