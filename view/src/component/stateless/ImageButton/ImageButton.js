b8bdb8dbac31e8b762bc595400563eace3257c54
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 20:18:23 +0200

    added custom style to Image button

--- view/src/component/stateless/ImageButton/ImageButton.js
+++ view/src/component/stateless/ImageButton/ImageButton.js
@@ -3,9 +3,10 @@ import classes from './ImageButton.module.css';
 export default function ImageButton(props) {
   const title = props.title;
   const src = props.imageSrc;
+  const style = props.customStyle;
   
   return (
-    <div className={classes.ImageButton}>
+    <div className={classes.ImageButton} style={style}>
       <h3>{title}</h3>
       <img src={src} alt='obraz'/>
     </div>
