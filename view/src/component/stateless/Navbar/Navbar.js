7767229359b0f84a5f630a0073b95051408591ab
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 18:19:20 +0200

    created new stateless component

---
+++ view/src/component/stateless/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import classes from './Navbar.module.css';
+
+export default function Navbar(props) {
+  return (
+    <nav className={classes.Navbar}>
+      <h1>Rejestr pasiek</h1>
+    </nav>
+  );
+}
