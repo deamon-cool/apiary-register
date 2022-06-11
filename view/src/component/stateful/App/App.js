1bc2394e83a2bbaa4cbfa917e1b35050ce1082a9
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 19:58:17 +0200

    imported ImageButtons to app

--- view/src/component/stateful/App/App.js
+++ view/src/component/stateful/App/App.js
@@ -2,13 +2,15 @@ import classes from './App.module.css';
 
 import Navbar from '../../stateless/Navbar/Navbar';
 import Container from '../../stateless/Container/Container';
+import ImageButton from '../../stateless/ImageButton/ImageButton';
 
 function App() {
   return (
     <div className={classes.App}>
       <Navbar />
       <Container>
-        
+        <ImageButton />
+        <ImageButton />
       </Container>
     </div>
   );
