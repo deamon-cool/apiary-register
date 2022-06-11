2b8fc4a8f7271923485408f442e6fd358b81af37
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 18:20:58 +0200

    implemented Navbar to App

--- view/src/component/stateful/App/App.js
+++ view/src/component/stateful/App/App.js
@@ -1,9 +1,11 @@
 import classes from './App.module.css';
 
+import Navbar from '../../stateless/Navbar/Navbar';
+
 function App() {
   return (
     <div className={classes.App}>
-      
+      <Navbar />
     </div>
   );
 }
