0ae11bd73c76c47ffd7bcfeaaf73a5f58bbf177c
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 09:19:04 +0200

    implemented controller in api

--- api/api.js
+++ api/api.js
@@ -3,12 +3,13 @@ const path = require('path');
 const config = require('./config/config.json');
 
 const addNewApiaryController = require('./controllers/addNewApiaryController');
+const getApiariesController = require('./controllers/getApiariesController');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.put('/api/add-new-apiary', addNewApiaryController);
 
-app.get('/api/get-apiaries', (req, res) => res.status(200).send(['pasieki']));
+app.get('/api/get-apiaries', getApiariesController);
 
 app.listen(config.API_PORT, () => console.log('Api runs on: 127.0.0.1:' + config.API_PORT));
