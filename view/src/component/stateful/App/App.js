bd9faccaeaf9c507a46aee8b250339c86af73ea1
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-11 20:41:56 +0200

    changed app component

--- view/src/component/stateful/App/App.js
+++ view/src/component/stateful/App/App.js
@@ -1,19 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
 import classes from './App.module.css';
 
 import Navbar from '../../stateless/Navbar/Navbar';
 import Container from '../../stateless/Container/Container';
-import ImageButton from '../../stateless/ImageButton/ImageButton';
-import newApiaryImage from '../../../assests/images/new-apiary.png';
-import apiaryListImage from '../../../assests/images/apiary-list.png';
 
 function App() {
   return (
     <div className={classes.App}>
       <Navbar />
       <Container>
-        <ImageButton title='Nowa pasieka' imageSrc={newApiaryImage} customStyle={{margin: '10px'}}/>
-        <ImageButton title='Lista pasiek' imageSrc={apiaryListImage} customStyle={{margin: '10px'}}/>
+        <Routes>
+
+        </Routes>
       </Container>
+      <RoutesContainer>
+        <ImageButton title='Nowa pasieka' imageSrc={newApiaryImage} customStyle={{ margin: '10px' }} />
+        <ImageButton title='Lista pasiek' imageSrc={apiaryListImage} customStyle={{ margin: '10px' }} />
+      </RoutesContainer>
     </div>
   );
 }
