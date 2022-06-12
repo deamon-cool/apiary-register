1c12cc0040fbc7f55d68ddf08e49618588e3e0da
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-12 10:36:45 +0200

    added navigations to ImageButton's in RoutesContainer

--- view/src/component/stateless/RoutesContainer/RoutesContainer.js
+++ view/src/component/stateless/RoutesContainer/RoutesContainer.js
@@ -1,14 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+
 import classes from './RoutesContainer.module.css';
 
+import { NEW_APIARY_ROUTE, LIST_OF_APIARIES_ROUTE } from '../../../config/config';
 import ImageButton from '../ImageButton/ImageButton';
 import newApiaryImage from '../../../assests/images/new-apiary.png';
 import apiaryListImage from '../../../assests/images/apiary-list.png';
 
 export default function RoutesContainer(props) {
+  let navigate = useNavigate();
+
   return (
     <div className={classes.RoutesContainer}>
-      <ImageButton title='Nowa pasieka' imageSrc={newApiaryImage} customStyle={{ margin: '10px' }} />
-      <ImageButton title='Lista pasiek' imageSrc={apiaryListImage} customStyle={{ margin: '10px' }} />
+      <ImageButton
+        onClickHandler={() => navigate(NEW_APIARY_ROUTE)}
+        title='Nowa pasieka'
+        imageSrc={newApiaryImage}
+        customStyle={{ margin: '10px' }} />
+      <ImageButton
+        onClickHandler={() => navigate(LIST_OF_APIARIES_ROUTE)}
+        title='Lista pasiek'
+        imageSrc={apiaryListImage}
+        customStyle={{ margin: '10px' }} />
     </div>
   );
 }
