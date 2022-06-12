bdee8473ea125804f4597ef993cc2841af36dccb
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-12 19:44:22 +0200

    added logic to Input component

--- view/src/component/stateless/Input/Input.js
+++ view/src/component/stateless/Input/Input.js
@@ -2,14 +2,18 @@ import classes from './Input.module.css';
 
 export default function Input(props) {
   const value = props.value;
+  const readOnly = props.readOnly;
+  const customStyle = props.customStyle;
 
   const changeHandler = (e) => {
     props.onChangeHandler(e.target.value);
   };
 
   return (
-    <input className={classes.InputDate}
+    <input className={classes.Input}
+      style={customStyle}
       onChange={changeHandler}
-      value={value} />
+      value={value} 
+      disabled={readOnly}/>
   );
 }
