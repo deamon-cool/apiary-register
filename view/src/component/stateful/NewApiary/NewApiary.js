5aa2e118f2c36843d571f5f8a8463c10a0b04925
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-12 11:44:39 +0200

    created new stateful component NewApiary to create apiaries

---
+++ view/src/component/stateful/NewApiary/NewApiary.js
@@ -0,0 +1,14 @@
+import classes from './NewApiary.module.css';
+
+export default function NewApiary() {
+
+  return (
+    <div className={classes.NewApiary}>
+      <form>
+        <label></label>
+        <input type='date'/>
+        
+      </form>
+    </div>
+  );
+}
