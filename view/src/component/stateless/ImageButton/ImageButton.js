79e893b5a99a9924fa6b90043bb99a6cd3c06063
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-12 10:35:44 +0200

    added click handler property to ImageButton

--- view/src/component/stateless/ImageButton/ImageButton.js
+++ view/src/component/stateless/ImageButton/ImageButton.js
@@ -4,11 +4,16 @@ export default function ImageButton(props) {
   const title = props.title;
   const src = props.imageSrc;
   const style = props.customStyle;
-  
+
+  const clickHandler = () => {
+    props.onClickHandler();
+  };
+
   return (
-    <div className={classes.ImageButton} style={style}>
+    <div className={classes.ImageButton} style={style}
+      onClick={clickHandler}>
       <h3>{title}</h3>
-      <img src={src} alt='obraz'/>
+      <img src={src} alt='obraz' />
     </div>
   );
 }
