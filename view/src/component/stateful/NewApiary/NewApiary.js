419957ea155266964e0b3950fb8b0ad1baa62530
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-12 12:02:36 +0200

    created default form in NewApiary

--- view/src/component/stateful/NewApiary/NewApiary.js
+++ view/src/component/stateful/NewApiary/NewApiary.js
@@ -4,10 +4,22 @@ export default function NewApiary() {
 
   return (
     <div className={classes.NewApiary}>
+      <h2>Nowa pasieka</h2>
       <form>
-        <label></label>
-        <input type='date'/>
-        
+        <fieldset>
+          <label>Nazwa</label>
+          <input />
+        </fieldset>
+        <fieldset>
+          <label>Wybierz datę</label>
+          <input type='date' />
+        </fieldset>
+        <fieldset>
+          <label>Numer pasieki</label>
+          2022-06-12<input />789
+        </fieldset>
+
+        <button>Zapisz</button>
       </form>
     </div>
   );
