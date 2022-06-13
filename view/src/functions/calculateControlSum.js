6c61d1f58fbd745d0eaa62ed6bc1b467a003cdb4
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-13 13:08:36 +0200

    created carulationControlSum function

--- view/src/functions/calculateControlSum.js
+++ view/src/functions/calculateControlSum.js
@@ -1,3 +1,27 @@
-export default function calculateControlSum() {
+export default function calculateControlSum(dateString, userApiaryNumber) {
+  if (dateString.length !== 8) {
+    return '';
+  }
 
+  if (userApiaryNumber.length !== 5) {
+    return '';
+  }
+
+  const apiaryNumberWithoutControlSumStr = [dateString, userApiaryNumber].join('');
+
+  let computedNumber = parseInt(apiaryNumberWithoutControlSumStr);
+  for (let i in apiaryNumberWithoutControlSumStr) {
+    const num = parseInt(apiaryNumberWithoutControlSumStr[i]);
+    if (num !== 0) {
+      computedNumber *= num;
+    }
+  }
+
+  const computedNumberStr = computedNumber.toString();
+
+  const computedControlSum = computedNumberStr.charAt(1) +
+    computedNumberStr.charAt(6) +
+    computedNumberStr.charAt(computedNumberStr.length - 1);
+
+  return computedControlSum;
 }
