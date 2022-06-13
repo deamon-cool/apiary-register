db9bcd27a6798ba1d0ea9f54bd0017465142716f
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-13 11:14:31 +0200

    added managing date value

--- view/src/component/stateful/NewApiary/NewApiary.js
+++ view/src/component/stateful/NewApiary/NewApiary.js
@@ -10,6 +10,17 @@ export default function NewApiary() {
   const [date, setDate] = useState(getDateString());
   const [userApiaryNumber, setUserApiaryNumber] = useState('');
 
+  const dateValue = date.split('-').join('');
+  function getDateString() {
+    const newDate = new Date();
+    const monthString = newDate.getMonth().toString().length === 1 ?
+      `0${newDate.getMonth() + 1}` : `${newDate.getMonth() + 1}`;
+    const dayString = newDate.getDate().toString().length === 1 ?
+      `0${newDate.getDate()}` : `${newDate.getDate()}`;
+    const dateString = `${newDate.getFullYear()}-${monthString}-${dayString}`;
+
+    return dateString;
+  }
   return (
     <div className={classes.NewApiary}>
       <h2>Nowa pasieka</h2>
