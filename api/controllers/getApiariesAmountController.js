39bea3c1ac0ff297ba77ad8a58ef4989870f7d71
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-13 15:51:15 +0200

    corrected controller getApiariesAmount

--- api/controllers/getApiariesAmountController.js
+++ api/controllers/getApiariesAmountController.js
@@ -5,8 +5,9 @@ module.exports = async (req, res) => {
     const { date } = req.params;
 
     const apiaries = await Apiary.find({ date: date });
+    const apiariesAmount = apiaries !== null ? apiaries.length : 0;
 
-    res.status(200).send({ apiariesAmount: apiaries.length });
+    res.status(200).send({ apiariesAmount: apiariesAmount });
   } catch (err) {
     console.log(Date() + ' an error has occured \n' + err);
     return res.status(500).send({ error: 'Błąd api. Przepraszamy.' });
