618d89cd9f50b90d2a2edc3ff903437eb9f2a2d6
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-13 12:19:30 +0200

    created external function for date validation

--- view/src/functions/validateDate.js
+++ view/src/functions/validateDate.js
@@ -1,4 +1,19 @@
 export default function validateDate(dateString) {
+  const splitedDateArr = date.split('-');
+  const year = parseInt(splitedDateArr[0]);
+  const month = parseInt(splitedDateArr[1]);
+  const day = parseInt(splitedDateArr[2]);
 
-  return 'message'
+  let monthDuration = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
+    monthDuration[1] = 29;
+  }
+
+  if (day > monthDuration[month - 1] || day < 1 ||
+    month > 12 || month < 1 ||
+    year > (new Date()).getFullYear()) {
+    return 'Nieprawidłowa data. Sprawdź dokładnie rok, miesiąc i dzień.';
+  }
+
+  return '';
 }
