3c918cc52c207791455244a7784311b279d769df
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-13 12:14:35 +0200

    deleted functions form NewApiary scope

--- view/src/component/stateful/NewApiary/NewApiary.js
+++ view/src/component/stateful/NewApiary/NewApiary.js
@@ -21,21 +21,6 @@ export default function NewApiary() {
   const dateValue = date.split('-').join('');
   const controlSum = calculateControlSum();
 
-  function getDateString() {
-    const newDate = new Date();
-    const monthString = newDate.getMonth().toString().length === 1 ?
-      `0${newDate.getMonth() + 1}` : `${newDate.getMonth() + 1}`;
-    const dayString = newDate.getDate().toString().length === 1 ?
-      `0${newDate.getDate()}` : `${newDate.getDate()}`;
-    const dateString = `${newDate.getFullYear()}-${monthString}-${dayString}`;
-
-    return dateString;
-  }
-
-  function calculateControlSum() {
-    return '786';
-  }
-
   const nameHandler = (name) => {
     if (name.length > 100) {
       setInputErrors(state => ({
