f53fa2514c7a57f1d6fe38b39159c12847254e36
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-13 20:17:50 +0200

    created new config constans for fetching new apiary

--- view/src/config/config.js
+++ view/src/config/config.js
@@ -2,6 +2,7 @@ export const NEW_APIARY_ROUTE = '/nowa-pasieka';
 export const LIST_OF_APIARIES_ROUTE = '/lista-pasiek';
 
 export const APIARIES_AMOUNT_AT_DATE_FETCH_URL = '/api/apiaries-amount/';
+export const NEW_APIARY_FETCH_URL = '/api/add-new-apiary';
 
 export const WARNING_TIME_SEK = 6;
 export const INFO_TIME_SEK = 6;
