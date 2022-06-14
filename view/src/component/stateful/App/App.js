dff057e5afec2199c0cf7ad4787b400473f3a733
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-14 08:00:43 +0200

    added Routes to App and added lazy loading to split code

--- view/src/component/stateful/App/App.js
+++ view/src/component/stateful/App/App.js
@@ -1,12 +1,16 @@
+import React, { Suspense } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import classes from './App.module.css';
 
 import { LIST_OF_APIARIES_ROUTE, NEW_APIARY_ROUTE } from '../../../config/config';
+
 import Navbar from '../../stateless/Navbar/Navbar';
 import Container from '../../stateless/Container/Container';
 import RoutesContainer from '../../stateless/RoutesContainer/RoutesContainer';
-import NewApiary from '../NewApiary/NewApiary';
+
+const NewApiary = React.lazy(() => import('../NewApiary/NewApiary'));
+const Apiaries = React.lazy(() => import('../Apiaries/Apiaries'));
 
 function App() {
   let navigate = useNavigate();
@@ -15,10 +19,13 @@ function App() {
     <div className={classes.App}>
       <Navbar onImageClick={() => navigate('/')} />
       <Container>
-        <Routes>
-          <Route path="/" element={<RoutesContainer />} />
-          <Route path={NEW_APIARY_ROUTE} element={<NewApiary />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<RoutesContainer />} />
+            <Route path={NEW_APIARY_ROUTE} element={<NewApiary />} />
+            <Route path={LIST_OF_APIARIES_ROUTE} element={<Apiaries />} />
+          </Routes>
+        </Suspense>
       </Container>
     </div>
   );
