46edee565573f05d6badee9af0370156ad459c11
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-14 08:47:20 +0200

    added handlers and states to Apiaries

--- view/src/component/stateful/Apiaries/Apiaries.js
+++ view/src/component/stateful/Apiaries/Apiaries.js
@@ -1,9 +1,43 @@
+import { useState } from 'react';
+
 import classes from './Apiaries.module.css';
 
+import Search from '../../stateless/Search/Search';
+import Button from '../../stateless/Button/Button';
+import DateRange from '../../stateless/DateRange/DateRange';
+
+import getDateString from '../../../functions/getDateString';
+import validateDate from '../../../functions/validateDate';
+
 export default function Apiaries() {
+  const [sorting, setSorting] = useState('');
+  const [customDateRange, setCustomDateRange] = useState({
+    from: getDateString(),
+    to: getDateString()
+  });
+
+  //download date
+
+  const sortHandler = () => {
+
+  };
+
+  const dateHandler = (name, value) => {
+
+  };
+
   return (
     <div className={classes.Apiaries}>
-
+      <Search>
+        <Button
+          onClickHandler={sortHandler}
+          text='Sortuj' />
+        <DateRange
+          onDateRangeHandler={dateHandler}
+          fromDate={customDateRange.from}
+          toDate={customDateRange.to} />
+        {/* clear search */}
+      </Search>
     </div>
   );
 }
