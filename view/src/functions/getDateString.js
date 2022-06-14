3145220a467a9fed081c83110ce3efaccf8a46e8
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-14 19:51:31 +0200

    modified function getDateString

--- view/src/functions/getDateString.js
+++ view/src/functions/getDateString.js
@@ -1,10 +1,9 @@
-export default function getDateString() {
-  const newDate = new Date();
-  const monthString = newDate.getMonth().toString().length === 1 ?
-    `0${newDate.getMonth() + 1}` : `${newDate.getMonth() + 1}`;
-  const dayString = newDate.getDate().toString().length === 1 ?
-    `0${newDate.getDate()}` : `${newDate.getDate()}`;
-  const dateString = `${newDate.getFullYear()}-${monthString}-${dayString}`;
+export default function getDateString(date) {
+  const monthString = date.getMonth().toString().length === 1 ?
+    `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+  const dayString = date.getDate().toString().length === 1 ?
+    `0${date.getDate()}` : `${date.getDate()}`;
+  const dateString = `${date.getFullYear()}-${monthString}-${dayString}`;
 
   return dateString;
 }
