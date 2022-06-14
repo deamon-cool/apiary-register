54b4770353ff9d46ea09520d071a149428b19000
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-14 08:21:08 +0200

    created lements in DateRange

--- view/src/component/stateless/DateRange/DateRange.js
+++ view/src/component/stateless/DateRange/DateRange.js
@@ -4,7 +4,13 @@ export default function DateRange(props) {
 
   return (
     <div className={classes.DateRange}>
-      
+      <Input
+        onChangeHandler={dateHandler}
+        value={customDate} />
+      <b>-</b>
+      <Input
+        onChangeHandler={dateHandler}
+        value={customDate} />
     </div>
   );
 }
