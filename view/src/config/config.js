8c73ecd0f036944d1f5d31f4fa37a209c5ba31a8
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-14 11:17:04 +0200

    added url constans to config for fetching apiaries

--- view/src/config/config.js
+++ view/src/config/config.js
@@ -2,6 +2,7 @@ export const NEW_APIARY_ROUTE = '/nowa-pasieka';
 export const LIST_OF_APIARIES_ROUTE = '/lista-pasiek';
 
 export const NEW_APIARY_FETCH_URL = '/api/add-new-apiary';
+export const GET_APIARIES_FETCH_URL = '/api/get-apiaries';
 
 export const WARNING_TIME_SEK = 6;
 export const INFO_TIME_SEK = 6;
