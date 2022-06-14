a1a3e3e83c7b3d2893578edb79a199a205e363be
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-14 18:04:16 +0200

    modified DateInput component

--- view/src/component/stateless/DateInput/DateInput.js
+++ view/src/component/stateless/DateInput/DateInput.js
@@ -1,7 +1,7 @@
 import classes from './DateInput.module.css';
 
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import "./customDatePicker.css";
 import { registerLocale } from "react-datepicker";
 import pl from 'date-fns/locale/pl';
 
@@ -9,17 +9,18 @@ registerLocale('pl', pl);
 
 export default function DateInput(props) {
   const date = props.date;
-  const customStyle = props.customStyle;
 
-  const changeHandler = (date) => {
-    props.onChangeHandler(date);
+  const changeDateHandler = (date) => {
+    props.onDateChangeHandler(date);
   };
 
   return (
     <DatePicker
+      className={classes.DatePicker}
       locale='pl'
+      dateFormat="yyyy-MM-dd"
       closeOnScroll={true}
       selected={date}
-      onChange={changeHandler} />
+      onChange={changeDateHandler} />
   );
 }
