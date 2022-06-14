36eeaaa7d49647c10e21bad91850b217192da771
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-14 21:19:17 +0200

    modified external validateDate function

--- view/src/functions/validateDate.js
+++ view/src/functions/validateDate.js
@@ -1,17 +1,17 @@
-export default function validateDate(dateString) {
-  const splitedDateArr = dateString.split('-');
-  const year = parseInt(splitedDateArr[0]);
-  const month = parseInt(splitedDateArr[1]);
-  const day = parseInt(splitedDateArr[2]);
+export default function validateDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
 
   let monthDuration = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
     monthDuration[1] = 29;
   }
 
+  const today = new Date();
+
   if (day > monthDuration[month - 1] || day < 1 ||
-    month > 12 || month < 1 ||
-    year > (new Date()).getFullYear()) {
+    month > 12 || month < 1 || date > today) {
     return 'Nieprawidłowa data. Sprawdź dokładnie rok, miesiąc i dzień.';
   }
 
