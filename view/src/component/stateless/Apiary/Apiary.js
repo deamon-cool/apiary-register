b53e802da24b9f30f3704285d0c219d17e629d7c
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-14 11:16:36 +0200

    added elements to Apiary

--- view/src/component/stateless/Apiary/Apiary.js
+++ view/src/component/stateless/Apiary/Apiary.js
@@ -1,11 +1,21 @@
 import classes from './Apiary.module.css';
 
+import newApiaryImage from '../../../assests/images/new-apiary.png';
+
 export default function Apiary(props) {
+  const name = props.name;
+  const date = props.date;
+  const apiaryNumber = props.apiaryNumber;
   const customStyle = props.customStyle;
 
   return (
     <article className={classes.Apiary} style={customStyle}>
-      {props.children}
+      <img src={newApiaryImage} alt='obraz' />
+      <section>
+        <h3>{name}</h3>
+        <p>{date}</p>
+        <p>{apiaryNumber}</p>
+      </section>
     </article>
   );
 }
