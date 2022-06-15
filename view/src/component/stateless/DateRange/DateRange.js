94526fee584cd405e4530bf8c9cca6a6765ebe8f
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-15 08:32:27 +0200

    created DateRange component

--- view/src/component/stateless/DateRange/DateRange.js
+++ view/src/component/stateless/DateRange/DateRange.js
@@ -1,32 +1,47 @@
 import classes from './DateRange.module.css';
 
-import Input from '../Input/Input';
-import DateInput from '../DateInput/DateInput';
+import DatePicker from "react-datepicker";
+import "../DateInput/CustomDatePicker.css";
+import { registerLocale } from "react-datepicker";
+import pl from 'date-fns/locale/pl';
+
+registerLocale('pl', pl);
 
 export default function DateRange(props) {
-  const fromDate = props.fromDate;
-  const toDate = props.toDate;
+  const startDate = props.startDate;
+  const endDate = props.endDate;
   const customStyle = props.customStyle;
 
-  const dateRangeHandler = (e) => {
-    props.onDateRangeHandler(e.target.name, e.target.value);
+  const changeStartDate = (date) => {
+    props.onChangeStartDate(date);
+  };
+
+  const changeEndDate = (date) => {
+    props.onChangeEndDate(date);
   };
 
   return (
     <div className={classes.DateRange} style={customStyle}>
-      <DateInput shorterWidth={true}/>
-      {/* <Input
-        name='fromDate'
-        onChangeHandler={dateRangeHandler}
-        value={fromDate}
-        customStyle={{width: '120px'}} /> */}
+      <DatePicker className={classes.DatePicker}
+        locale='pl'
+        dateFormat="yyyy-MM-dd"
+        closeOnScroll={true}
+        selectsStart
+        selected={startDate}
+        onChange={changeStartDate}
+        startDate={startDate}
+        endDate={endDate} />
       <b>-</b>
-      <DateInput shorterWidth={true}/>
-      {/* <Input
-        name='toDate'
-        onChangeHandler={dateRangeHandler}
-        value={toDate}
-        customStyle={{width: '120px'}} /> */}
+      <DatePicker className={classes.DatePicker}
+        locale='pl'
+        dateFormat="yyyy-MM-dd"
+        closeOnScroll={true}
+        selectsEnd
+        selected={endDate}
+        onChange={changeEndDate}
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate} />
     </div>
   );
 }
