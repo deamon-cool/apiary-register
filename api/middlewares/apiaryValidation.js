3fda95a9030d839ba2a83050f0bef439a1205506
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-15 13:52:20 +0200

    corrected apiaryValidation middleware to check actual date

--- api/middlewares/apiaryValidation.js
+++ api/middlewares/apiaryValidation.js
@@ -40,9 +40,11 @@ module.exports = async (req, res, next) => {
       monthDuration[1] = 29;
     }
 
+    const today = new Date();
+    const sentDate = new Date(date);
+
     if (day > monthDuration[month - 1] || day < 1 ||
-      month > 12 || month < 1 ||
-      year > (new Date()).getFullYear()) {
+      month > 12 || month < 1 || sentDate > today) {
       return res.status(400).send({ error: 'Nieprawidłowa data. Sprawdź dokładnie rok, miesiąc i dzień.' });
     }
 
